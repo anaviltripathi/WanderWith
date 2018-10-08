3fb707521bfe8803267512a10ac50d2cd17c16da
anaviltripathi <anaviltripathi@gmail.com>
2018-10-07 23:48:30 -0700

    project setup

---
+++ app/src/androidTest/java/com/example/anavil/wanderwith/ExampleInstrumentedTest.kt
@@ -0,0 +1,24 @@
+package com.example.anavil.wanderwith
+
+import android.support.test.InstrumentationRegistry
+import android.support.test.runner.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getTargetContext()
+        assertEquals("com.example.anavil.wanderwith", appContext.packageName)
+    }
+}
